0292828e18b0be83b0fefcd27ed13ffee97223e9
Paul Denlinger <pmdenlinger@icloud.com>
2021-06-19 14:27:07 -0700

    Add ExploreView, CameraView.

---
+++ instagram-app/Views/CameraView.swift
@@ -0,0 +1,20 @@
+//
+//  CameraView.swift
+//  instagram-app
+//
+//  Created by user on 6/19/21.
+//
+
+import SwiftUI
+
+struct CameraView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CameraView_Previews: PreviewProvider {
+    static var previews: some View {
+        CameraView()
+    }
+}
---
+++ instagram-app/Views/ExploreView.swift
@@ -0,0 +1,20 @@
+//
+//  ExploreView.swift
+//  instagram-app
+//
+//  Created by user on 6/19/21.
+//
+
+import SwiftUI
+
+struct ExploreView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ExploreView_Previews: PreviewProvider {
+    static var previews: some View {
+        ExploreView()
+    }
+}
