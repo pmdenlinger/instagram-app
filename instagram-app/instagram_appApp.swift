74235357b9c6bcf716104c5b7a0d5ebfc7f12a32
Paul Denlinger <pmdenlinger@icloud.com>
2021-06-19 14:21:05 -0700

    Initial Commit

---
+++ instagram-app/instagram_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  instagram_appApp.swift
+//  instagram-app
+//
+//  Created by user on 6/19/21.
+//
+
+import SwiftUI
+
+@main
+struct instagram_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
