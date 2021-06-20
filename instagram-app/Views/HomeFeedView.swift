41f861a860cf8733bbf63adb8917c740e2c1133d
Paul Denlinger <pmdenlinger@icloud.com>
2021-06-19 17:39:24 -0700

    Add PostView struct

--- instagram-app/Views/HomeFeedView.swift
+++ instagram-app/Views/HomeFeedView.swift
@@ -27,8 +27,63 @@ struct HomeFeedView: View {
     }
 }
 
+struct PostView: View {
+    let userImageName: String
+    let imageName: String
+    
+    var body:  some View {
+        VStack {
+            PostHeaderView(userImageName: userImageName)
+                .padding()
+            
+//            Image
+            Image(imageName)
+                .resizable()
+                .frame(width: 430, height: 430, alignment: .center)
+                .aspectRatio(contentMode: .fill)
+                .background(Color(.secondarySystemBackground))
+            
+            PostActionButtonsView()
+            
+            HStack {
+//                like count
+                Image(systemName: "heart.fill")
+                    .resizable()
+                    .foregroundColor(Color.red)
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 30, height: 30, alignment: .center)
+                
+                Text("32 Likes")
+                    .font(.system(size: 20)
+                    .foregroundColor(foregroundColor(.link))
+                Spacer()
+            }
+            .padding()
+            .padding(.top, -20)
+            
+//            Caption
+            Text("Oh man I hope 2021 is better than 2020! #newyear #trending #swiftui")
+                .font(.headline)
+                .padding(.top, -15)
+            
+//            Comments
+            
+            HStack {
+//                Date
+                Text("1 hour ago")
+                    .foregroundColor(Color(.secondaryLabel))
+                    .multilineTextAlignment(.leading)
+                Spacer()
+            }
+            .padding()
+        }
+    }
+    
+}
+
 struct HomeFeedView_Previews: PreviewProvider {
     static var previews: some View {
         HomeFeedView()
+            .preferredColorScheme(.dark)
     }
 }
